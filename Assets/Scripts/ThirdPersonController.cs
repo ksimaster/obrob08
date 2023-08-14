06c8db515bde218d909bb6ecc3a1a2a72e82a481
ksimaster <ksimasterlord@gmail.com>
2023-08-14 19:51:16 +0300

    fix controller

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -114,10 +114,11 @@ namespace StarterAssets
         {
             get
             {
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED && UNITY_WEBGL && UNITY_EDITOR
                 return _playerInput.currentControlScheme == "KeyboardMouse";
 #else
-				return false;
+                return _playerInput.currentControlScheme == "KeyboardMouse";
+               // return false;
 #endif
             }
         }
@@ -139,10 +140,11 @@ namespace StarterAssets
             _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<StarterAssetsInputs>();
-#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED && UNITY_WEBGL && UNITY_EDITOR
             _playerInput = GetComponent<PlayerInput>();
 #else
-			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
+            _playerInput = GetComponent<PlayerInput>();
+            Debug.Log( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
 
             AssignAnimationIDs();
