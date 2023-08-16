4414eb2cc8e893e5c8eb8cccf372030542517eca
ksimaster <ksimasterlord@gmail.com>
2023-08-16 15:10:40 +0300

    fix death

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
-
+    public GameObject panelDeath; 
 
     private void OnTriggerEnter(Collider col)
     {
         Debug.Log(col.gameObject.name);
         if (col.gameObject.CompareTag("Laser"))
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+            Time.timeScale = 0;
+            panelDeath.SetActive(true);
+            if (panelDeath.activeSelf) 
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            } 
+            // дописать отключалки времени, взаимодействия и т.п.
         }
     }
 }
